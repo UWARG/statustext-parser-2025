e36fb396f0cfcf8225fa1bac991934b009b21306
Tq <tqmeng123@gmail.com>
2025-01-17 01:14:53 +0000

    Try this

--- modules/recieve_statustext.py
+++ modules/recieve_statustext.py
@@ -34,7 +34,7 @@ def main(save_directory: str, document_name_prefix: str) -> int:
     """
     save_directory = Path(args.save_directory)
     document_name_prefix = args.document_name_prefix
-    
+
     vehicle = mavutil.mavlink_connection(CONNECTION_ADDRESS, source_system=255, source_component=0)
     # Catch library and other unexpected errors
     # pylint: disable=broad-exception-caught
@@ -57,37 +57,42 @@ def main(save_directory: str, document_name_prefix: str) -> int:
             success, worker_id, expected_positions_count = (
                 metadata_encoding_decoding.decode_metadata(msg.data)
             )
-            if not success or worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
-                return
+            if not success:
+                return -1
+            if worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
+                continue
             received_positions_count = 0
             while received_positions_count < expected_positions_count:
                 gps_msg = vehicle.recv_match(type="STATUSTEXT", blocking=True)
                 if not gps_msg:
                     print("no message")
-                    continue 
+                    continue
                 if gps_msg.get_type() == "BAD_DATA":
                     if mavutil.all_printable(gps_msg.data):
                         print(gps_msg.data)
-                    return -1 
+                    return -1
                 success, worker_id, global_position = (
                     message_encoding_decoding.decode_bytes_to_position_global(gps_msg.data)
-                ) 
-                if not success or worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
-                    print(f"Unsuccessful or Non-communications worker message (worker_id: {worker_id})")
-                    return -1 
+                )
+                if not success:
+                    return -1
+                if worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
+                    continue
                 print(
                     f"Decoded GPS Data: {global_position.latitude}, {global_position.longitude}, {global_position.altitude}"
-                ) 
+                )
                 positions.append(global_position)
-                received_positions_count += 1 
+                received_positions_count += 1
             success, kml_path = kml_conversion.positions_to_kml(
                 positions, document_name_prefix, save_directory
-            ) 
+            )
             if not success:
                 print("Failed to save KML file")
                 return -1
             print(f"KML file saved to {kml_path}")
-    return 0 
+    return 0
+
+
 DEFAULT_SAVE_DIRECTORY = "logs"
 DEFAULT_DOCUMENT_NAME_PREFIX = "IR hotspot locations"
 if __name__ == "__main__":
