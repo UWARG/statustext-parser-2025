2b1f13ca483b82f371d83718d0e9bbaff70ae978
Tq <tqmeng123@gmail.com>
2025-01-20 19:40:13 +0000

    LOG

--- modules/recieve_statustext.py
+++ modules/recieve_statustext.py
@@ -10,7 +10,7 @@ from pymavlink import mavutil
 from modules.common.modules.data_encoding import message_encoding_decoding
 from modules.common.modules.data_encoding import metadata_encoding_decoding
 from modules.common.modules.data_encoding import worker_enum
-import modules.common.modules.kml
+from modules.common.modules.kml import kml_conversion
 
 
 CONNECTION_ADDRESS = "tcp:localhost:14550"
@@ -38,6 +38,7 @@ def main(save_directory: str, document_name_prefix: str) -> int:
         vehicle.wait_heartbeat()
         print("connected")
     except Exception as e:
+
         print(f"Error connecting to vehicle: {e}")
         return -1
     while True:
@@ -45,47 +46,57 @@ def main(save_directory: str, document_name_prefix: str) -> int:
         msg = vehicle.recv_match(type="STATUSTEXT", blocking=True)
         if not msg:
             print("no message")
-        elif msg.get_type() == "BAD_DATA":
+            continue
+        if msg.get_type() == "BAD_DATA":
             if mavutil.all_printable(msg.data):
-                print(msg.data)
+                print(f"Bad data received: {msg.data}")
+            print("Error: Bad data")
             return -1
-        else:
-            success, worker_id, expected_positions_count = (
-                metadata_encoding_decoding.decode_metadata(msg.data)
+
+        # Receive first metadata message from communications worker to determine number of hotspots
+        success, worker_id, expected_positions_count = metadata_encoding_decoding.decode_metadata(
+            msg.data
+        )
+        if not success:
+            print("Error: Failed to decode metadata")
+            return -1
+        if worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
+            continue
+        received_positions_count = 0
+
+        # Collecting all hotspot locations
+        while received_positions_count < expected_positions_count:
+            gps_msg = vehicle.recv_match(type="STATUSTEXT", blocking=True)
+            if not gps_msg:
+                print("No GPS message received.")
+                continue
+            if gps_msg.get_type() == "BAD_DATA":
+                if mavutil.all_printable(gps_msg.data):
+                    print(f"Bad GPS data received: {gps_msg.data}")
+                print("Error: Bad GPS data")
+                return -1
+            success, worker_id, global_position = (
+                message_encoding_decoding.decode_bytes_to_position_global(gps_msg.data)
             )
             if not success:
+                print("Error: Failed to decode GPS data")
                 return -1
             if worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
                 continue
-            received_positions_count = 0
-            while received_positions_count < expected_positions_count:
-                gps_msg = vehicle.recv_match(type="STATUSTEXT", blocking=True)
-                if not gps_msg:
-                    print("no message")
-                    continue
-                if gps_msg.get_type() == "BAD_DATA":
-                    if mavutil.all_printable(gps_msg.data):
-                        print(gps_msg.data)
-                    return -1
-                success, worker_id, global_position = (
-                    message_encoding_decoding.decode_bytes_to_position_global(gps_msg.data)
-                )
-                if not success:
-                    return -1
-                if worker_id != worker_enum.WorkerEnum.COMMUNICATIONS_WORKER:
-                    continue
-                print(
-                    f"Decoded GPS Data: {global_position.latitude}, {global_position.longitude}, {global_position.altitude}"
-                )
-                positions.append(global_position)
-                received_positions_count += 1
-            success, kml_path = modules.common.modules.kml.kml_conversion.positions_to_kml(
-                positions, document_name_prefix, save_directory
+            print(
+                f"Decoded GPS Data: {global_position.latitude}, {global_position.longitude}, {global_position.altitude}"
             )
-            if not success:
-                print("Failed to save KML file")
-                return -1
-            print(f"KML file saved to {kml_path}")
+            positions.append(global_position)
+            received_positions_count += 1
+
+        # Generating KML file
+        success, kml_path = kml_conversion.positions_to_kml(
+            positions, document_name_prefix, save_directory
+        )
+        if not success:
+            print("Failed to save KML file")
+            return -1
+        print(f"KML file saved to {kml_path}")
     return 0
 
 
@@ -108,4 +119,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    main(args.save_directory, args.document_name_prefix)
+    if main(args.save_directory, args.document_name_prefix) == 0:
+        print("Process completed successfully.")
+    else:
+        print("Process failed.")
